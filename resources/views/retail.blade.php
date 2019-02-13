ef02e3d4559709f8675de076828d51d2e6462781
Peter Tiernan <ptiernan@gmail.com>
2019-02-13 16:27:47 +0000

    starting on retail page

--- resources/views/retail.blade.php
+++ resources/views/retail.blade.php
@@ -5,7 +5,19 @@
 @endsection
 
 @section('content')
+<div class="retail-title-container">
+    <div class="retail-title">
+        <h1 class="retail-title-heading">Retail</h1>
+    </div>
+</div>
+<div class="retail-image-container">
+    <div class="retail-image">
+        <img src="/images/shop.jpg" alt="">
+        <h2 class="retail-image-title">Come and visit our genuine factory shop with ample free parking!</h2>
+    </div>
+</div>
 
+<img src="/images/retail_1.png" alt="">
 @endsection
 
 @section('scripts')

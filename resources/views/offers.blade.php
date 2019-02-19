8a9b257da0f2a91d7e6cbadc960b464a95eddb20
Peter Tiernan <ptiernan@gmail.com>
2019-02-19 23:32:46 +0000

    creating offers page

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -6,6 +6,97 @@
 
 @section('content')
 
+<div class="offers-banner-container bg-p">
+    <div class="offers-banner my-m">
+        <h1 class="offers-banner-item my-m c-g1 fs-xl">
+            Offers From O'Manony's
+        </h1>
+    </div>
+</div>
+
+<div class="latest-offer-container bg-g2">
+    <div class="latest-offer">
+        <div class="latest-offer-title my-m">
+            <div class="latest-offer-title-item fs-l c-p">
+                Latest Offer
+            </div>
+        </div>
+
+        <div class="latest-offer-card my-l">
+            <div class="latest-offer-card-left">
+                <img src="/images/gallery7.jpg" alt="">
+            </div>
+            <div class="latest-offer-card-right px-m py-m bg-g1">
+                <p class="latest-offer-card-right-details fs-m">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores consequatur porro sapiente pariatur, quos fuga.</p>
+                <h2 class="latest-offer-card-right-valid c-p ">Valid Until: </h2>
+            </div>
+            <div class="latest-offer-card-bottom bg-p c-g1">
+                <h3 class="latest-offer-card-bottom-item py-s fs-l">Offer Title</h3>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="offers-container">
+    <div class="offers my-l">
+        <div class="offers-card">
+            <div class="offers-card-img">
+                <img src="/images/gallery9.jpg" alt="">
+            </div>
+            <div class="offers-card-title bg-p c-g1 px-s py-s fs-m">
+                <h4 class="offers-card-title-item">
+                    Offer Title
+                </h4>
+            </div>
+            <div class="offers-card-detail">
+                <p class="offers-card-detail-item my-m px-s">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque ratione repudiandae porro, autem illo atque?</p>
+            </div>
+            <div class="offers-card-bottom bg-s">
+                <h4 class="offers-card-bottom-item py-s c-g1 px-s">
+                    Valid Until: 
+                </h4>
+            </div>
+        </div>
+        <div class="offers-card">
+            <div class="offers-card-img">
+                <img src="/images/gallery9.jpg" alt="">
+            </div>
+            <div class="offers-card-title bg-p c-g1 px-s py-s fs-m">
+                <h4 class="offers-card-title-item">
+                    Offer Title
+                </h4>
+            </div>
+            <div class="offers-card-detail">
+                <p class="offers-card-detail-item my-m px-s">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque ratione repudiandae porro, autem illo atque?</p>
+            </div>
+            <div class="offers-card-bottom bg-s">
+                <h4 class="offers-card-bottom-item py-s c-g1 px-s">
+                    Valid Until: 
+                </h4>
+            </div>
+        </div>
+        <div class="offers-card">
+            <div class="offers-card-img">
+                <img src="/images/gallery9.jpg" alt="">
+            </div>
+            <div class="offers-card-title bg-p c-g1 px-s py-s fs-m">
+                <h4 class="offers-card-title-item">
+                    Offer Title
+                </h4>
+            </div>
+            <div class="offers-card-detail">
+                <p class="offers-card-detail-item my-m px-s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nam voluptatibus non repellat laudantium, molestiae voluptates ex odit laborum rerum, quasi quam reiciendis, dicta dolorum odio! </p>
+            </div>
+            <div class="offers-card-bottom bg-s">
+                <h4 class="offers-card-bottom-item py-s c-g1 px-s">
+                    Valid Until: 
+                </h4>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 @section('scripts')

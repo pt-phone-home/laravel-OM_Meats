db5dba09d013d5aa1ede8aedac2f616ec9b913f7
Peter Tiernan <ptiernan@gmail.com>
2019-03-08 14:33:42 +0000

    minor tweaks

--- app/Http/Controllers/OffersController.php
+++ app/Http/Controllers/OffersController.php
@@ -57,22 +57,28 @@ class OffersController extends Controller
             'valid' => 'required'
         ]);
 
+        $offer = Offer::find($id);
+
         if ($request->file('img')) {
             $filenameWithExt = $request->file('img')->getClientOriginalName();
             $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
             $extension = $request->file('img')->getClientOriginalExtension();
             $filenameToStore = $filename . '_' . time() . '.' . $extension;
             $path = $request->file('img')->move('images/offers', $filenameToStore);
-        } else {
-            $path = 'images/offers/default' . rand(1, 4) . '.jpg';
-        }
 
-        $offer = Offer::find($id);
+            $offer->img = $path;
+
+        } 
+        // else {
+        //     $path = 'images/offers/default' . rand(1, 4) . '.jpg';
+        // }
+
+        
 
         $offer->title = $request['title'];
         $offer->valid = $request['valid'];
         $offer->detail = $request['detail'];
-        $offer->img = $path;
+        
 
         $offer->save();
         return redirect('admin')->with('success', 'Offer Added Successfully');

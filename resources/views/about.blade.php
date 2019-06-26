1b8b0145696fdc03d440181e3a60f7c85e88c330
Peter Tiernan <ptiernan@gmail.com>
2019-06-26 11:45:07 +0100

    adding new picture

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -37,7 +37,7 @@ About | O'Mahony Meats Ltd.
     <div class="container mx-auto flex flex-wrap pt-12 pb-6">
         <div class="w-full md:w-1/3 py-2">
             <div class="h-20r px-4 py-4 bg-transparent md:bg-grey-light">
-                <img src="/images/OM_old.jpg" alt="" class="h-full w-full object-cover">
+                <img src="/images/OM_old.png" alt="" class="h-full w-full object-cover">
             </div>
         </div>
         <div class="w-full md:w-2/3 py-2 px-6 lg:px-12 flex items-center">

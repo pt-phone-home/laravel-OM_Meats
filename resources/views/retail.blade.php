5a9adca2e3c4d4ec9f04d3c8e8cec6476295eef1
Peter Tiernan <ptiernan@Peters-MacBook-Pro.local>
2020-06-10 22:00:12 +0100

    adding brochure

--- resources/views/retail.blade.php
+++ resources/views/retail.blade.php
@@ -28,6 +28,56 @@
     <div class="w-1/5 sm:w-1/4 lg:w-1/3">
 
     </div>
+    <h1 class="text-grey-lightest bgi-s py-6 w-3/5 sm:w-1/2 lg:w-1/3 text-center rounded-t-xlg font-nun font-light uppercase">
+        New Delivery Service
+    </h1>
+    <div class="w-1/5 sm:w-1/4 lg:w-1/3">
+
+    </div>
+</div>
+<div class="bgi-s">
+    <div class="container mx-auto flex flex-wrap py-12">
+        <div class="w-full md:w-2/3 py-2 px-12 flex items-center">
+            <div class="flex flex-col justify-center items-start">
+                <p class="py-4 text-grey-lightest text-2xl">
+                   How to order?
+                </p>
+                <div class="text-grey-lightest">
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>By phone (01 8476000 +1)</p>
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>By email (orders@ommeats.ie)</p>
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>Via Facebook/Messenger</p>
+                </div>
+                <p class="py-4 text-grey-lightest text-2xl">
+                   How can you pay?
+                </p>
+                <div class="text-grey-lightest">
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>By card over the phone (ahead of delivery)</p>
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>You must supply a contact number when ordering!</p>
+                </div>
+                <p class="py-4 text-grey-lightest text-2xl">
+                    PLEASE NOTE:
+                </p>
+                <div class="text-grey-lightest">
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>€40 minimum value, a €5 delivery charge will be added to you order.</p>
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>FREE delivery for orders over €80.</p>
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>We deliver Monday-Friday in the Dublin area.</p>
+                    <p class="py-2 text-xl"><i class="fas fa-ellipsis-h text-p mr-4"></i>All orders must be paid PRIOR to delivery</p>
+                </div>
+            </div>
+        </div>
+        <div class="w-full md:w-1/3 py-2 flex flex-col items-center justify-center">
+            <h2 class="text-grey-lightest font-normal text-center mb-2">Click to view and download our delivery brochure</h2>
+            <div class="h-20r px-4 py-4 bg-transparent md:bg-grey-light">
+                <a href="{{asset('delivery_brochure.pdf')}}" target="_blank"><img src="/images/brochure_cover.png" alt=""
+                                                                         class="h-full w-full object-cover"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="div bgi-s pt-8 flex">
+    <div class="w-1/5 sm:w-1/4 lg:w-1/3">
+
+    </div>
     <h1 class="text-grey-darkest bgi-g2 py-6 w-3/5 sm:w-1/2 lg:w-1/3 text-center rounded-t-xlg font-nun font-light uppercase">
         Large Choice of Products
     </h1>
